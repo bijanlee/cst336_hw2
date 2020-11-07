6122b8602207b9f7ae0480c4afc4eadde218b160
EC2 Default User <ec2-user@ip-172-31-26-43.us-west-1.compute.internal>
2020-11-07 19:31:57 +0000

    full functionality finished

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
     var item2Cost = 4.99;
     var item3Cost = 4.99;
     
+    //array for shopping cart items
+    var shoppingCartItems = [];
+    
     $("#item1Buy").on("click", function() {
         var quantity = $("#item1Qty").val();
         addItem("Carton of Eggs", 1, quantity);
@@ -27,39 +30,92 @@ $(document).ready(function(){
     //productNumber, quantity, cost
     function addItem(productName, productNumber, quantity) {
         
-        var itemCost = 0;
-        var itemTotal = 0;
-        //hardcoded values for now
-        if(productNumber == 1){
-            itemCost = item1Cost;
-            itemTotal = item1Cost * quantity;
-        }else if(productNumber == 2) {
-            itemCost = item2Cost;
-            itemTotal = item2Cost * quantity;
-        }else if(productNumber == 3) {
-            itemCost = item3Cost;
-            itemTotal = item3Cost * quantity;
+        var quantityInt = parseInt(quantity);
+        
+        var shoppingCartItem = 
+        {
+            "id" : productNumber,
+            "productName": productName,
+            "quantity": quantityInt
         }
         
-        var htmlToAppend = `<tr id="row${rowNumber}">`;
-        htmlToAppend += `<td>${productName}</td>`;
-        htmlToAppend += `<td>${quantity}</td>`;
-        htmlToAppend += `<td>${itemCost}</td>`;
-        htmlToAppend += `<td>${itemTotal}</td>`;
-        htmlToAppend += `<td><button type="button" class="btn btn-primary" id="delete${rowNumber}">Delete</button></td>`;
-        htmlToAppend += `</tr>`;
+        var found = false;
+        var index = -1;
+        for(var i = 0; i < shoppingCartItems.length; i++) {
+            if(shoppingCartItems[i].id == productNumber) {
+                shoppingCartItems[i].quantity += shoppingCartItem.quantity;
+                found = true;
+                index = i;
+                break;
+            }
+        }
+        if(found == false){
+            //add to shopping cart array
+            //add new table row
+            shoppingCartItems.push(shoppingCartItem);
         
-        $('#shoppingCartTable tbody:last-child').append(htmlToAppend);
-        var deleteId = `#row${rowNumber}`;
-        $("#shoppingCartTable").on('click', deleteId, function() {
-            removeItem(deleteId);
-        })
+            var itemCost = 0;
+            var itemTotal = 0;
+            //hardcoded values for now
+            if(productNumber == 1){
+                itemCost = item1Cost;
+                itemTotal = item1Cost * quantityInt;
+            }else if(productNumber == 2) {
+                itemCost = item2Cost;
+                itemTotal = item2Cost * quantityInt;
+            }else if(productNumber == 3) {
+                itemCost = item3Cost;
+                itemTotal = item3Cost * quantityInt;
+            }
+            
+            var htmlToAppend = `<tr id="row${rowNumber}">`;
+            htmlToAppend += `<td>${productName}</td>`;
+            htmlToAppend += `<td id="quantity${rowNumber}">${quantityInt}</td>`;
+            htmlToAppend += `<td id="itemCost${rowNumber}">${itemCost}</td>`;
+            htmlToAppend += `<td id="itemTotal${rowNumber}">${itemTotal}</td>`;
+            htmlToAppend += `<td><button type="button" class="btn btn-primary" id="delete${rowNumber}">Delete</button></td>`;
+            htmlToAppend += `</tr>`;
+            
+            $('#shoppingCartTable tbody:last-child').append(htmlToAppend);
+            var deleteId = `#row${rowNumber}`;
+            $("#shoppingCartTable").on('click', deleteId, function() {
+                removeItem(deleteId, productNumber);
+            })
+            rowNumber++;
+        }else {
+            //update existing
+            var quantityInt = shoppingCartItems[index].quantity;
+            var itemCost = 0;
+            var itemTotal = 0;
+            //hardcoded values for now
+            if(productNumber == 1){
+                itemCost = item1Cost;
+                itemTotal = item1Cost * quantityInt;
+            }else if(productNumber == 2) {
+                itemCost = item2Cost;
+                itemTotal = item2Cost * quantityInt;
+            }else if(productNumber == 3) {
+                itemCost = item3Cost;
+                itemTotal = item3Cost * quantityInt;
+            }
+            var quantitySelector = `#quantity${index+1}`;
+            var itemTotalSelector = `#itemTotal${index+1}`;
+            $(quantitySelector).html(quantityInt);
+            $(itemTotalSelector).html(itemTotal);
+        }
+        
     }
     
     //remove item
     
-    function removeItem(id) {
+    function removeItem(id, productId) {
         $(id).remove();
+        for(var i = 0; i < shoppingCartItems.length; i++) {
+            if(shoppingCartItems[i].id == productId) {
+                shoppingCartItems.splice(i, 1);
+            }
+        }
+        rowNumber--;
     }
     
     //update item
